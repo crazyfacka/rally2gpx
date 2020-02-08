daddbc3f3cf1063dbb297b0accbab04ddba5f0d1
crazyfacka <gmail@joagonca.com>
2020-02-08 19:53:56 +0000

    Promisify data check and add eslint config

--- index.js
+++ index.js
@@ -2,23 +2,42 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 const virtualConsole = new jsdom.VirtualConsole();
-virtualConsole.on("error", (err) => { console.log(err) });
+virtualConsole.on('error', (err) => { console.log(err); });
+
+/* AUX FUNCTIONS */
+
+const checkData = function (dom) {
+  return new Promise((resolve, reject) => {
+    let count = 0;
+    const check = setInterval(function (w) {
+      count++;
+      if (typeof w.sl.leaflet !== 'undefined') {
+        console.log(`Found ${w.sl.leaflet.data.storage.stages.length} tracks`);
+        clearInterval(check);
+        resolve(w.sl.leaflet);
+      }
+
+      if (count > 10) {
+        clearInterval(check);
+        reject(new Error('Timed out waiting for resources'));
+      }
+    }, 1000, dom.window);
+  });
+};
+
+/* THE MACHINE */
 
 console.log('Downloading and parsing data from URL');
 
 JSDOM.fromURL('https://www.rally-maps.com/Rally-de-Portugal-2019/Vieira-do-Minho', {
-    includeNodeLocations: true,
-    resources: "usable",
-    runScripts: "dangerously",
-    pretendToBeVisual: true,
-    virtualConsole,
-}).then(dom => {
-    let check = setInterval(function(w) {
-        if (typeof w.sl.leaflet !== 'undefined') {
-            console.log(`Found ${w.sl.leaflet.data.storage.stages.length} tracks`)
-            clearInterval(check);
-        }
-    }, 1000, dom.window);
-}).catch(err => {
+  includeNodeLocations: true,
+  resources: 'usable',
+  runScripts: 'dangerously',
+  pretendToBeVisual: true,
+  virtualConsole
+}).then(dom => checkData(dom))
+  .then(data => {
+    console.log(data);
+  }).catch(err => {
     console.log(err);
-});
+  });
